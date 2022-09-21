d971d9beeda41b4ff2eb49c997f832fe9f1ccdf0
IEalpha <iealpha@outlook.com>
2022-09-21 22:46:27 +0800

    组合回溯

--- LeetCode/src/backTracking/Combine.java
+++ LeetCode/src/backTracking/Combine.java
@@ -1,67 +1,62 @@
 package backTracking;
 
-/**
- * @author LiuHanxu
- * @Classname Combain
- * @Description TODO
- * @Date 2022/9/20 17:08
- */
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
  * 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
- *
+ * <p>
  * 你可以按 任何顺序 返回答案。
- *
+ * <p>
  *  
- *
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入：n = 4, k = 2
  * 输出：
  * [
- *   [2,4],
- *   [3,4],
- *   [2,3],
- *   [1,2],
- *   [1,3],
- *   [1,4],
+ * [2,4],
+ * [3,4],
+ * [2,3],
+ * [1,2],
+ * [1,3],
+ * [1,4],
  * ]
  * 示例 2：
- *
+ * <p>
  * 输入：n = 1, k = 1
  * 输出：[[1]]
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode.cn/problems/combinations
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
+
 public class Combine {
+
+    List<List<Integer>> resultList = new ArrayList<>();
+    LinkedList<Integer> linkedList = new LinkedList<>();
+
     public static void main(String[] args) {
         int n = 4, k = 2;
-        List<List<Integer>> result = new Combine().combine(n, k);
-        System.out.println(result);
+        System.out.println(new Combine().combine(n, k));
     }
 
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> result = new ArrayList<>();
-        LinkedList<Integer> list = new LinkedList<>();
-        combine(n, k, 1, result, list);
-        return result;
+        combine(n, k, 1);
+        return resultList;
     }
 
-    public void combine(int n, int k, int start, List<List<Integer>> result, LinkedList<Integer> list) {
-        if (list.size() == k) {
-            result.add(new ArrayList<>(list));
+    public void combine(int n, int k, int start) {
+        if (linkedList.size() == k) {
+            resultList.add(new ArrayList<>(linkedList));
             return;
         }
-        for (int i = start; i <= n; i++) {
-            list.add(i);
-            combine(n, k, i + 1, result, list);
-            list.removeLast();
+        for (int i = start; i <= n + 1 + linkedList.size() - k; i++) {
+            linkedList.add(i);
+            combine(n, k, i + 1);
+            linkedList.removeLast();
         }
     }
 }
